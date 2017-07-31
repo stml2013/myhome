f2608a1db27a6304760c1a24321528b71205ff75
stml2013 <wypsmtl2013@gmail.com>
2017-08-01 00:01:06 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,44 @@
+!/usr/bin/bash
+
+prefix=$PWD
+if [ "xx$1" != "xx" ]; then
+    prefix=$1
+fi
+
+tmp_dir=$prefix/tmp
+zlib_path=$tmp_dir/zlib_
+openssl_path=$tmp_dir/openssl_
+pcre_path=$tmp_dir/pcre_
+
+if [ ! -d "$tmp_dir" ]; then
+    mkdir $tmp_dir
+fi
+
+if [ ! -d "$zlib_path" ]; then
+    wget http://zlib.net/zlib-1.2.8.tar.gz
+    tar -zxf zlib-1.2.8.tar.gz
+    mv zlib-1.2.8 $zlib_path 
+fi
+
+if [ ! -d "$openssl_path" ]; then
+    wget https://www.openssl.org/source/openssl-1.0.1t.tar.gz
+    tar -zxf openssl-1.0.1t.tar.gz
+    mv openssl-1.0.1t $openssl_path 
+fi
+
+if [ ! -d "$pcre_path" ]; then
+    wget http://downloads.sourceforge.net/project/pcre/pcre/8.35/pcre-8.35.tar.gz
+    tar -zxf pcre-8.35.tar.gz
+    mv pcre-8.35 $pcre_path
+fi
+
+cd ./nginx_1015
+
+./configure --prefix=$prefix \
+    --with-openssl=$openssl_path \
+    --with-pcre=$pcre_path \
+    --with-http_stub_status_module \
+    --with-http_ssl_module
+
+make 
+sudo make install
